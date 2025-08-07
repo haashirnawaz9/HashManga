b71892a1ae9096b537f52fdf85530ec9f320b16f
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-08-07 14:03:56 -0400

    Your commit message here

--- server/src/server.js
+++ server/src/server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3001
 const app = express();
 MongoDB();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://hashmanga.netlify.app',
+    credentials: true,
+}));
+
 app.use(express.json())
 
 app.use('/auth', userRouter);
