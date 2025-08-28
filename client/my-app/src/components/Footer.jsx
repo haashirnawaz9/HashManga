01a6a44e14f97155769db0ecd728ea5785fe3c0a
haashirnawaz9 <153224457+haashirnawaz9@users.noreply.github.com>
2025-08-28 15:40:20 -0400

    Update project with latest changes

--- client/my-app/src/components/Footer.jsx
+++ client/my-app/src/components/Footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
       <footer className="mt-auto bottom-0 h-10">
         {/* Full-bleed bar that ignores parent max-width/padding */}
         <div className="relative left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] w-screen
-                        border-t border-gray-900">
+                        border-t border-neutral-800 shadow-lg">
           {/* Centered content area */}
           <div className="max-w-7xl mx-auto p-6 text-center justify-center">
-            <h1 className="">Copyright © 2025 HashManga. All Rights reserved.</h1>
+            <h1 className="text-neutral-400">Copyright © 2025 HashManga. All Rights reserved.</h1>
           </div>
         </div>
       </footer>
